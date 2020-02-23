9705fb3ab695c81285b398b7c052b022633ec298
skessler <selwyn.kessler79@gmail.com>
2020-02-23 22:51:54 +0800

    initial_commit

---
+++ Course_1/Week1/Practice_Exercise_for_Expressions.py
@@ -0,0 +1,33 @@
+#
+# Week 1 Practice Exercises 
+#
+
+# q1
+print(5280 * 13)
+
+# q2
+print(7 * 3600 + 21 * 60 + 37)
+
+# q3
+print(4 * 2 + 7 * 2)
+
+# q4
+print(4 * 7)
+
+# q5
+print(2 * 3.14 * 8)
+
+# q6
+print(3.14 * 8 ** 2)
+
+# q7
+print(1000 * (1 + 0.01 * 7) ** 10)
+
+# q8
+print("My name is" + " " + "Joe" + " " + "Warren" + ".")
+
+# q9
+print("Joe Warren is " + str(56) + " years old.")
+
+# q10 (challenge)
+print(((2-5)**2 + (2-6)**2) ** 0.5)
